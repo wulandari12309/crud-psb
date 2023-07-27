760c5043c1a4d1e7dda3e685536c4510c5bab873
wulandari12309 <104313969+wulandari12309@users.noreply.github.com>
2023-07-27 17:11:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h4>Pendaftaran Siswa Baru</h4>
+    <h1>Digital Talent</h1>
+    <br><br>
+    <h4>Menu</h4>
+    <ul>
+        <li><a href="formDaftar.php">Daftar Baru</a></li>
+        <li><a href="pendaftar.php">Pendaftar</a></li>
+    </ul>
+</body>
+</html>
